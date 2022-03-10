1a5bdcbb8eff137652149d9ebb284cd73df8c697
epitaq <somaok2004@gmail.com>
2022-03-10 16:10:34 +0900

    json作成

--- create_json.py
+++ create_json.py
@@ -1,19 +1,25 @@
 import json
 import csv
 
-video_list = []
-count = 0
-title = []
-with open ('videoList.csv','r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        if count == 0:
-            title = row
-            count += 1
-        else:
-            count += 1
-            video_dic = {title[i] : row[i] for i in range(len(title))}
-            video_list.append(video_dic)
-video_json = json.dumps({'contens':video_list}, indent=4)
-    
-print(video_json)
+def main () :
+    video_list = []
+    count = 0
+    title = []
+    with open ('videoList.csv','r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if count == 0:
+                title = row
+                count += 1
+            else:
+                count += 1
+                video_dic = {title[i] : row[i] for i in range(len(title))}
+                video_list.append(video_dic)
+    # JSONの作成＆保存
+    video_json = json.dumps({'contents':video_list}, indent=4)
+    # print(video_json)
+    fw = open('videoList.json','w')
+    json.dump({'contents':video_list}, fw, indent=4)
+
+if __name__ == '__main__':
+    main()
