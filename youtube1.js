f017ac77a785969528e359da889cb6ac1d2a4b31
epitaq <somaok2004@gmail.com>
2022-03-10 09:45:34 +0900

    omoitukanai

--- youtube1.js
+++ youtube1.js
@@ -14,10 +14,48 @@ function onYouTubeIframeAPIReady() {
         width: '640',
         // videoId: 'M7lc1UVf-VE',
         events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        },
-        // playerVars: {
-        // }
+            onReady: ready2,
+        }
     });
+}
+
+var videoId = ['M7lc1UVf-VE','68KV7JnrvDo','glbYC6rBn3g'];
+var videoStart = [10, 2, 4]
+var videoEnd = [14, 5, 5]
+
+function ready () {
+    for (let i=0; i<videoId.length; i++ ) {
+        console.log(i)
+        console.log(videoId[i])
+        player.loadVideoById({
+            'videoId': videoId[i],
+            'startSeconds': videoStart[i],
+            'endSeconds': videoEnd[i],
+            'suggestedQuality': 'small'});
+    }
+};
+
+// var videoList = [['M7lc1UVf-VE',10,14], ['68KV7JnrvDo',2,5], ['glbYC6rBn3g',4,6]]
+// function ready1 () {
+//     for await (var element of videoList){
+//         setTimeout(stop, 3000)
+//         console.log(element[0])
+//     }
+// }
+function stop(){}
+
+function ready2 () {
+    for (let i=0; i<videoId.length; i++ ) {
+        setTimeout(timeStop(i), 3000)
+    }
+};
+
+function timeStop (i){
+    console.log(i)
+    console.log(videoId[i])
+    player.loadVideoById({
+        'videoId': videoId[i],
+        'startSeconds': videoStart[i],
+        'endSeconds': videoEnd[i],
+        'suggestedQuality': 'small'});
 }
