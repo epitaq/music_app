b034447db5b90c59e8fe92d7758ebc3b153c46fa
epitaq <somaok2004@gmail.com>
2022-03-10 20:50:17 +0900

    json作成

--- create_json_6.py
+++ create_json_6.py
@@ -2,29 +2,37 @@ import json
 import csv
 
 def main () :
-    video_list = []
     count = 0
-    title = []
     with open ('videoList1.csv','r', encoding="utf-8") as f:
         reader = csv.reader(f)
         for row in reader:
+            print(row)
             if count == 0:
-                title = row
+                # 辞書のキーの部分
+                field_name = row
                 count += 1
             else:
-                count += 1
-                video_dic = {title[i] : row[i] for i in range(len(title))}
-                video_list.append(video_dic)
-    # print(video_list)
-    # JSONの作成
-    video_all = [video_list[i]['id'] for i in range(len(video_list))]
-    print(video_all)
-    video_list = {'contents':video_list, 'videoList':video_all}
-    video_json = json.dumps(video_list, indent=4)
-    # JSONの保存
-    fw = open('videoList.json','w')
-    # json.dump(video_list, fw, indent=4)
-    print(video_json)
+                # ユニークなid
+                id = row[1] + '-' + row[4]
+                # youtubeの動画のId
+                movie = row[1]
+                # 歌手
+                name = row[2]
+                # 歌名
+                title = row[3]
+                # スタートの時間(s)
+                start = 0
+                s_time_list = row[4].split(':')
+                for i in range(len(s_time_list)):
+                    start += int(s_time_list[(-1*i)-1]) * (60**i)
+                # 終わりの時間(s)
+                end = 0
+                e_time_list = row[5].split(':')
+                for i in range(len(e_time_list)):
+                    end += int(e_time_list[(-1*i)-1]) * (60**i)
+
+
+
 
 if __name__ == '__main__':
     main()
