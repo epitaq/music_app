d4691a4705f0beb4db9c9af68d52c0a57efd2022
epitaq <somaok2004@gmail.com>
2022-03-13 19:21:30 +0900

    分数つきのcsvを秒数に変換

---
+++ create_MusicList_csv.py
@@ -0,0 +1,35 @@
+import csv
+
+csv_name = 'html/videoList3のコピー.csv'
+music_list = []
+with open(csv_name, 'r') as f:
+    data = csv.reader(f)
+    for i in data:
+        if i[0] == 'id':
+            pass
+        else:
+            # スタートを秒数に変換
+            start = 0
+            start_list = i[4].split(':')
+            for n in range(len(start_list)):
+                start += int(start_list[(-1*n)-1]) * (60**n)
+            i[4] = start
+            # エンドを秒数に変換
+            end = 0
+            end_list = i[5].split(':')
+            for n in range(len(end_list)):
+                end += int(end_list[(-1*n)-1]) * (60**n)
+            i[5] = end
+            #
+        # print(i)
+        music_list.append(i)
+
+# エクスポート
+with open(csv_name, 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(music_list)
+
+
+
+
+
