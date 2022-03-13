7f06621d59015a028db89fdd4a13dfadd3c6def9
epitaq <somaok2004@gmail.com>
2022-03-13 21:41:48 +0900

    移植

--- mysite/musicapp/views.py
+++ mysite/musicapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import MusicList
+
+
+def index(request):
+    template = loader.get_template('musicapp/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
