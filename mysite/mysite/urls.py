862fc946535c18a8915fad953eb667c70f024a55
epitaq <somaok2004@gmail.com>
2022-03-14 17:00:45 +0900

    ?

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('player/', include('musicapp.urls')),
     path('admin/', admin.site.urls),
-    path('/', admin.site.urls),
+    path('', include('musicapp.urls')),
 ]
