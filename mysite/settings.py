03ae338929c6bb2c215d0b393f063e90bf9a35aa
epitaq <somaok2004@gmail.com>
2022-03-14 22:38:11 +0900

    コピペのままだった

--- mysite/settings.py
+++ mysite/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django_blog.context_processors.google_analytics',  # Googleアナリティクス
+                'mysite.context_processors.google_analytics',  # Googleアナリティクス
             ],
         },
     },
