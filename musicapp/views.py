83b376f0faa9ed5487d8372373962ea321920f58
epitaq <somaok2004@gmail.com>
2022-03-15 16:24:55 +0900

    jsonの送信テスト

--- musicapp/views.py
+++ musicapp/views.py
@@ -2,9 +2,40 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import MusicList
+import json
 
 
 def index(request):
     template = loader.get_template('musicapp/index.html')
-    context = {}
+    music_data = {
+            "content": [
+                {
+                    "id": "bQBHMPmgX8w-9:20",
+                    "movie": "bQBHMPmgX8w",
+                    "name": "ときのそら",
+                    "title": "KumoHurray!",
+                    "start": 560,
+                    "end": 744
+                },
+                {
+                    "id": "bQBHMPmgX8w-53:46",
+                    "movie": "bQBHMPmgX8w",
+                    "name": "ときのそら",
+                    "title": "花時の空",
+                    "start": 3226,
+                    "end": 3563
+                },
+                {
+                    "id": "3YfI8tPsfS4-12:02",
+                    "movie": "3YfI8tPsfS4",
+                    "name": "ときのそら",
+                    "title": "Step and Go!!",
+                    "start": 722,
+                    "end": 980
+                },
+            ]
+        }
+    context = {
+        'musicData': json.dumps(music_data , ensure_ascii=False)
+    }
     return HttpResponse(template.render(context, request))
