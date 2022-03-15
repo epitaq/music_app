69013d83093c77cb32418f404704171e32a7b2e0
epitaq <somaok2004@gmail.com>
2022-03-15 16:42:33 +0900

    jsonの受信に成功

--- musicapp/views.py
+++ musicapp/views.py
@@ -7,35 +7,33 @@ import json
 
 def index(request):
     template = loader.get_template('musicapp/index.html')
-    music_data = {
-            "content": [
-                {
-                    "id": "bQBHMPmgX8w-9:20",
-                    "movie": "bQBHMPmgX8w",
-                    "name": "ときのそら",
-                    "title": "KumoHurray!",
-                    "start": 560,
-                    "end": 744
-                },
-                {
-                    "id": "bQBHMPmgX8w-53:46",
-                    "movie": "bQBHMPmgX8w",
-                    "name": "ときのそら",
-                    "title": "花時の空",
-                    "start": 3226,
-                    "end": 3563
-                },
-                {
-                    "id": "3YfI8tPsfS4-12:02",
-                    "movie": "3YfI8tPsfS4",
-                    "name": "ときのそら",
-                    "title": "Step and Go!!",
-                    "start": 722,
-                    "end": 980
-                },
-            ]
-        }
+    music_data = [
+        {
+            "id": "bQBHMPmgX8w-9:20",
+            "movie": "bQBHMPmgX8w",
+            "name": "ときのそら",
+            "title": "KumoHurray!",
+            "start": 560,
+            "end": 744
+        },
+        {
+            "id": "bQBHMPmgX8w-53:46",
+            "movie": "bQBHMPmgX8w",
+            "name": "ときのそら",
+            "title": "花時の空",
+            "start": 3226,
+            "end": 3563
+        },
+        {
+            "id": "3YfI8tPsfS4-12:02",
+            "movie": "3YfI8tPsfS4",
+            "name": "ときのそら",
+            "title": "Step and Go!!",
+            "start": 722,
+            "end": 980
+        },]
+        
     context = {
-        "data": json.dumps(music_data , ensure_ascii=False)
+        "data": music_data
     }
     return HttpResponse(template.render(context, request))
