5a0ce69616413d2988b469e9c70826b457ad3829
epitaq <somaok2004@gmail.com>
2022-03-15 20:33:10 +0900

    部分的な一致

--- musicapp/views.py
+++ musicapp/views.py
@@ -10,10 +10,11 @@ def index(request):
 
     music_list = MusicList.objects
     #getの確認＋dbを叩く
+    # 部分的な一致を検索
     if 'name' in request.GET:
-        music_list = music_list.filter(name=request.GET['name'])
+        music_list = music_list.filter(name__contains=request.GET['name'])
     if 'title' in request.GET:
-        music_list = music_list.filter(title=request.GET['title'])
+        music_list = music_list.filter(title__contains=request.GET['title'])
     #dataを渡す
     music_data = list(music_list.values())
     context = {
