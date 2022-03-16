13de4f8ce9b166b01e634149fea3abfd1f53ff86
epitaq <somaok2004@gmail.com>
2022-03-16 16:15:55 +0900

    dbを拡張

--- musicapp/models.py
+++ musicapp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class MusicList(models.Model) :
     # id = models.CharField(max_length=20) #ユニークなID 
     movie = models.CharField(max_length=11) #動画ID
-    name = models.CharField(max_length=20) #歌った人
-    title = models.CharField(max_length=50) #歌名
+    name = models.CharField(max_length=256) #歌った人
+    title = models.CharField(max_length=256) #歌名
     start = models.IntegerField() #スタートの時間(s)
     end = models.IntegerField() #終わりの時間(s)
     def __str__(self):
