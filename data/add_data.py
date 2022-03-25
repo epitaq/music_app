5f6c7e345860bdd77512137d90e89f0bf22ece73
epitaq <somaok2004@gmail.com>
2022-03-25 23:43:20 +0900

    add data

--- data/add_data.py
+++ data/add_data.py
@@ -18,7 +18,7 @@ import csv
 # 追加するタグをリスト型で保存
 tag = ['アニメ','切り抜き','茅野愛衣']
 # 追加するcsvのpathのリスト デプロイがめんどくさかった
-csv_paths = ['data/otiai.csv']
+csv_paths = ['data/otiai1.csv']
 
 for csv_path in csv_paths:
     # 現在登録されているタグ
