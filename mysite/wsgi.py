9ba2071d32a0b47faf4bf79a6d5a876d3db70631
epitaq <somaok2004@gmail.com>
2022-05-25 20:35:35 +0900

    condaで環境を作成

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -15,10 +15,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 # application = get_wsgi_application()
 
+# https://qiita.com/Daisuke0209/items/059b0c53283dcf6536e8 より
+# https://qiita.com/neco0128_/items/5c637025c2ed6db5d80d 変わったらしい？
 import os
-from dj_static import Cling
+# from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
